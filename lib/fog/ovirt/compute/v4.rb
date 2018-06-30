2bab0d652b1b1645645010f568df705c208ea0a0
Ori Rabin <orrabin@gmail.com>
2018-06-30 21:23:18 +0300

    Add update_vm request to v4

--- lib/fog/ovirt/compute/v4.rb
+++ lib/fog/ovirt/compute/v4.rb
@@ -10,6 +10,7 @@ module Fog
         request :vm_action
         request :destroy_vm
         request :create_vm
+        request :update_vm
         request :datacenters
         request :storage_domains
         request :list_virtual_machines
