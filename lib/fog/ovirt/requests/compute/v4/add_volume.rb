35fc65dfd6e5b7778de9c8ab859a4149929fa964
Shira Maximov <shiramaximov@gmail.com>
2018-11-20 16:31:52 +0200

    Refs #20394 - Send the volume parameters in template

--- lib/fog/ovirt/requests/compute/v4/add_volume.rb
+++ lib/fog/ovirt/requests/compute/v4/add_volume.rb
@@ -26,6 +26,7 @@ module Fog
             options[:provisioned_size] ||= 8 * Fog::Compute::Ovirt::DISK_SIZE_TO_GB
             options[:type] ||= OvirtSDK4::DiskType::DATA
             options[:format] ||= OvirtSDK4::DiskFormat::COW
+
             options[:sparse] = true unless options[:sparse].present?
             options[:quota] = options[:quota].present? ? client.system_service.data_centers_service.data_center_service(datacenter).quotas_service.quota_service(options[:quota]).get : nil
 
@@ -33,8 +34,8 @@ module Fog
             options[:disk][:storage_domains] ||= [client.system_service.storage_domains_service.storage_domain_service(options[:storage_domain_id]).get]
             options[:disk][:provisioned_size] ||= options.delete(:provisioned_size)
             options[:disk][:format] ||= options.delete(:format)
-            options[:disk][:sparse] ||= options.delete(:sparse) == "true"
-            options[:disk][:wipe_after_delete] ||= options.delete(:wipe_after_delete) == "true"
+            options[:disk][:sparse] ||= options.delete(:sparse)
+            options[:disk][:wipe_after_delete] ||= options.delete(:wipe_after_delete)
             options
           end
           # rubocop:enable Metrics/AbcSize
--- lib/fog/ovirt/requests/compute/v4/list_template_volumes.rb
+++ lib/fog/ovirt/requests/compute/v4/list_template_volumes.rb
@@ -3,15 +3,22 @@ module Fog
     class Ovirt
       class V4
         class Real
+          # rubocop:disable Metrics/AbcSize
           def list_template_volumes(template_id)
             template = client.system_service.templates_service.template_service(template_id).get
-
             attachments = client.follow_link(template.disk_attachments)
-
             attachments.map do |attachment|
-              ovirt_attrs client.follow_link(attachment.disk)
+              attachment_disk = client.follow_link(attachment.disk)
+              attachment = client.follow_link(attachment)
+              bootable = attachment.bootable
+              interface = attachment.interface
+              attachment_disk.bootable = bootable if attachment_disk.bootable.nil?
+              attachment_disk.interface = interface if attachment_disk.interface.nil?
+              attachment_disk.storage_domain = attachment_disk.storage_domains[0].id if attachment_disk.storage_domains[0].present?
+              ovirt_attrs attachment_disk
             end
           end
+          # rubocop:enable Metrics/AbcSize
         end
         class Mock
           def list_template_volumes(_template_id)
