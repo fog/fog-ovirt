a69676883f0f0ecc20974516161ae9067c463513
Ori Rabin <orabin@redhat.com>
2019-03-09 10:03:52 +0200

    Adding get_volume request

---
+++ lib/fog/ovirt/requests/compute/v4/get_volume.rb
@@ -0,0 +1,19 @@
+module Fog
+  module Compute
+    class Ovirt
+      class V4
+        class Real
+          def get_volume(id)
+            ovirt_attrs client.system_service.disks_service.disk_service(id).get
+          end
+        end
+        class Mock
+          def get_volume(_id)
+            xml = read_xml("disk.xml")
+            ovirt_attrs OvirtSDK4::Reader.read(Nokogiri::XML(xml).root.to_s)
+          end
+        end
+      end
+    end
+  end
+end
